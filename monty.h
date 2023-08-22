51d6170df44f785896d4596ac324ecad386cf54c
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-22 23:03:20 +0100

    setting up the header file

---
+++ monty.h
@@ -0,0 +1,71 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+
+#define _POSIX_C_SOURCE 200809L
+
+#define INSTRUCTIONS		\
+	{
+		{"push", push},	\
+		{"pall", pall},	\
+		{NULL, NULL}	\
+	}
+
+/**
+ * struct stack_s - doubly linkedlist representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linkedlist node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - the opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * struct help - argument for the current opcode
+ * @data_struct: the stack mode, stack is set to be default
+ * @argument: the argument of the string
+ *
+ * Description: global structure used to pass data arount the function easily
+ */
+typedef struct help_s
+{
+	int data_struct;
+	char *argument;
+} help_t;
+
+
+help_t global;
+
+
+
+
+#endif /* MONTY_H */
