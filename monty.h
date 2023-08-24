82c950c60ecac09abee40be335f24e0a7c8abad5
EngrBassey <basseyleads001@gmail.com>
2023-08-24 10:52:54 -0700

    add nop func

--- monty.h
+++ monty.h
@@ -17,6 +17,7 @@
 		{"pop", pop}, \
 		{"swap", swap}, \
 		{"add", add}, \
+		{NULL, nop}, \
 		{NULL, NULL}	\
 	}
 
@@ -78,6 +79,7 @@ void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)));
 void pop(stack_t **stack, unsigned int line_cnt);
 void swap(stack_t **stack, unsigned int line_cnt);
 void add(stack_t **stack, unsigned int line_cnt);
+void nop(stack_t **stack, unsigned int line_cnt);
 
 /******************** errors_and_free.c **********************/
 void file_err(char *argv);
