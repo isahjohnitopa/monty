4dc2ea5f9eed9c36b04c4a24eefe6f2652d91fc5
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-24 12:17:44 +0100

    updates the mul opcode

--- mul.c
+++ mul.c
@@ -9,15 +9,12 @@
  */
 void _mul(stack_t **stack, unsigned int line_cnt)
 {
-	int result;
-
 	if (!stack || !(*stack) || !((*stack)->next))
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
 
-	result = ((*stack)->next->n) * ((*stack)->n);
+	(*stack)->next->n *= (*stack)->n;
 	pop(stack, line_cnt);
-	(*stack)->n = result;
 }
