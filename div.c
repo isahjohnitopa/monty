3d8ce798bd14eda5c85ee797e5b41c5cf46a4c8b
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-24 12:40:42 +0100

    updates the div opcode

--- div.c
+++ div.c
@@ -10,6 +10,8 @@
  */
 void _div(stack_t **stack, unsigned int line_cnt)
 {
+	int divisor = (*stack)->n;
+	int divident = (*stack)->next->n;
 	int result;
 
 	if (!stack || !*stack || !((*stack)->next))
@@ -17,13 +19,17 @@ void _div(stack_t **stack, unsigned int line_cnt)
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	if (((*stack)->n) == 0)
+	if (divisor == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	result = ((*stack)->next->n) / ((*stack)->n);
-	pop(stack, line_cnt);
-	(*stack)->n = result;
 
+	result = divident / divisor;
+
+	if (divisor < 0 && divident % divisor != 0)
+		result -= 1;
+
+	(*stack)->next->n = result;
+	pop(stack, line_cnt);
 }
